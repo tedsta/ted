9f6f82180bd63d5e95c37856a1b6e664503d1c5e
Theodore DeRego <tderego94@gmail.com>
2015-08-05 23:22:29 -1000

    Made all the commands and their inverse function

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,24 @@
 use buffer::Cursor;
 
 pub enum Command {
-    InsertLine(usize, String),
+    InsertLines(usize, Vec<String>),
     Insert(Cursor, String),
-    Remove(Cursor, Cursor),
+    Remove(Cursor, Cursor, String),
+    RemoveLines(usize, usize, Vec<String>),
+}
+
+impl Command {
+    pub fn inverse(self) -> Command {
+        use self::Command::*;
+
+        match self {
+            InsertLines(line, lines) => RemoveLines(line, line+lines.len()-1, lines),
+            Insert(cursor, text) => {
+                let end = Cursor { line: cursor.line, column: cursor.column+text.len() };
+                Remove(cursor, end, text)
+            },
+            Remove(start, _, text) => Insert(start, text),
+            RemoveLines(start, _, lines) => InsertLines(start, lines),
+        }
+    }
 }
