b555c47840c18c6b7fda5fd70ad9a4b0b55d8317
Theodore DeRego <tderego94@gmail.com>
2015-08-28 09:43:57 -1000

    Added InsertChar and RemoveChar so we don't have to store a whole string for the rather common case of inserting a character

--- src/operation.rs
+++ src/operation.rs
@@ -2,7 +2,9 @@ use cursor::Cursor;
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum Operation {
+    InsertChar(usize, char),
     Insert(usize, String),
+    RemoveChar(usize, char),
     Remove(usize, usize, String),
 }
 
@@ -11,7 +13,9 @@ impl Operation {
         use self::Operation::*;
 
         match self {
+            InsertChar(index, c) => RemoveChar(index, c),
             Insert(index, text) => Remove(index, index+text.len()-1, text),
+            RemoveChar(index, c) => InsertChar(index, c),
             Remove(start, _, text) => Insert(start, text),
         }
     }
