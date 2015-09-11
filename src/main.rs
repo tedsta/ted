c0b369dfe9f111397131bfc0e5233ead6a10b005
Theodore DeRego <tderego94@gmail.com>
2015-09-11 11:09:52 -1000

    We're going to use clap for cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(convert)]
 #![feature(drain)]
 
+#[macro_use]
+extern crate clap;
+
 extern crate bincode;
 extern crate rustc_serialize;
 extern crate rustbox;
@@ -20,6 +23,19 @@ mod ted_client;
 mod ted_server;
 
 fn main() {
+	let yml = load_yaml!("cli.yml");
+    let m = clap::App::from_yaml(yml).get_matches();
+
+    if let Some(mode) = m.value_of("mode") {
+        match mode {
+            "fast" => println!("We're really going now!"),
+            "slow" => println!("Awwww, too slow :("),
+            _      => unreachable!()
+        }
+    } else {
+        println!("--mode <MODE> wasn't used...");
+    }
+
     // Run our client editor
     Editor::from_string("fooobarrrr\nyumm I like cheese\ncheese loves me too <3.".to_string()).run();
 }
